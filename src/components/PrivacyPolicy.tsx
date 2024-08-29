182ff02981b8a8512eb3a57c22f847e1ef85c583
Anshu Singh <anshusinghofficial16@gmail.com>
2024-08-30 02:29:58 +0530

    removed div box from privacy policy page

--- src/components/PrivacyPolicy.tsx
+++ src/components/PrivacyPolicy.tsx
@@ -8,9 +8,6 @@ const PrivacyPolicy = () => {
     <div className="my-10">
     <div className="text-center py-8 text-3xl">
       <Title text1={"PRIVACY"} text2={"POLICY"} />
-    
-    </div>
-    <div className="flex flex-col sm:flex-row border border-gray-400 h-auto lg:h-[450px]">
     </div>
  <PrivacyPolicyItem/>
   </div>
