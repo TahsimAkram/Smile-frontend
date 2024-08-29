9d26cc88101d83a059882607b455c2a83e0cdb2f
Anshu Singh <anshusinghofficial16@gmail.com>
2024-08-30 02:30:22 +0530

    remove div box

--- src/components/PrivacyPolicy.tsx
+++ src/components/PrivacyPolicy.tsx
@@ -1,17 +1,17 @@
 import { assets } from "../assets/assets";
-import React  from "react";
+import React from "react";
 import Title from "./Title";
 import PrivacyPolicyItem from "./PrivacyPolicyItem";
 
 const PrivacyPolicy = () => {
   return (
     <div className="my-10">
-    <div className="text-center py-8 text-3xl">
-      <Title text1={"PRIVACY"} text2={"POLICY"} />
+      <div className="text-center py-8 text-3xl">
+        <Title text1={"PRIVACY"} text2={"POLICY"} />
+      </div>
+      <PrivacyPolicyItem />
     </div>
- <PrivacyPolicyItem/>
-  </div>
-  )
-}
+  );
+};
 
-export default PrivacyPolicy
+export default PrivacyPolicy;
